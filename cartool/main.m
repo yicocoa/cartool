008bf11490708f4b31c68532980ce70e81e35f16
yicocoa <110363676+yicocoa@users.noreply.github.com>
2022-09-20 11:44:35 +0800

    Update main.m

--- cartool/main.m
+++ cartool/main.m
@@ -150,13 +150,25 @@ void exportCarFileAtPath(NSString * carPath, NSString *outputDirectoryPath)
 	
 	outputDirectoryPath = [outputDirectoryPath stringByExpandingTildeInPath];
 	
-	CUIThemeFacet *facet = [CUIThemeFacet themeWithContentsOfURL:[NSURL fileURLWithPath:carPath] error:&error];
+	 //CUIThemeFacet *facet = [CUIThemeFacet themeWithContentsOfURL:[NSURL fileURLWithPath:carPath] error:&error];
 	
-	CUICatalog *catalog = [[CUICatalog alloc] init];
+	//CUICatalog *catalog = [[CUICatalog alloc] init];
 	
 	/* Override CUICatalog to point to a file rather than a bundle */
-	[catalog setValue:facet forKey:@"_storageRef"];
-	
+	//[catalog setValue:facet forKey:@"_storageRef"];
+    
+    CUICatalog *catalog = nil;
+    if ([CUICatalog instancesRespondToSelector:@selector(initWithURL:error:)]) {
+     //If CUICatalog has the URL API (Mojave), use it. */
+    catalog = [[CUICatalog alloc] initWithURL:[NSURL fileURLWithPath:carPath] error:&error];
+    } else {
+    CUIThemeFacet *facet = [CUIThemeFacet themeWithContentsOfURL:[NSURL fileURLWithPath:carPath] error:&error];
+    catalog = [[CUICatalog alloc] init];
+    // Override CUICatalog to point to a file rather than a bundle /
+    [catalog setValue:facet forKey:@"_storageRef"];
+    }
+    NSCAssert(!error, @"Error attempting to open asset catalog (%@): %@", carPath, error);
+
 	/* CUICommonAssetStorage won't link */
 	CUICommonAssetStorage *storage = [[NSClassFromString(@"CUICommonAssetStorage") alloc] initWithPath:carPath];
 	
